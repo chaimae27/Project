1618f87eb1a3455cb4d39f962052583c32a32d30
chaimae27 <40474422+chaimae27@users.noreply.github.com>
2019-05-21 10:12:13 +0000

    Update db.php

--- Admin/db.php
+++ Admin/db.php
@@ -2,7 +2,7 @@
 $localName = 'localhost';
 $dbname = 'project';
 $user = 'root';
-$pwd = 'balibla123';
+$pwd = 'password';
 try{
     $db = new PDO('mysql:host='.$localName.';dbname='.$dbname,$user,$pwd);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
